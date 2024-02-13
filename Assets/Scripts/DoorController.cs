81f520f019faa4b8dc52d11942d062b4bea88935
NethulG <157777700+NethulG@users.noreply.github.com>
2024-02-13 17:44:09 +1100

    Started door script

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,24 +8,31 @@ public class DoorController : MonoBehaviour
 {
     // Start is called before the first frame update
     public string SceneToLoad;
-    public bool isEntrance;
+    public bool isDoor;
+    public bool isKey;
     private Animator animator;
+    
+    public float numKey;
+    private float keysCollected = 0f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && isEntrance)
+        if (isDoor)
         {
-            Debug.Log("Player has entered");
-            animator.SetTrigger(AnimationStrings.PlayerEnter);
-            
+
+        }
+
+        if (isKey)
+        {
+
         }
     }
+
 
 
 
