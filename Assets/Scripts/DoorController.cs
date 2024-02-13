a953567f73c3a365de063f51c4c4bce2b5ebe840
NethulG <157777700+NethulG@users.noreply.github.com>
2024-02-13 18:53:29 +1100

    Door continued

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 #pragma warning disable
 
 public class DoorController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public string SceneToLoad;
+   
+    public string SceneToLoad; //identify the scene
+    
+    //identification of key or door
     public bool isDoor;
     public bool isKey;
     private Animator animator;
     
+    //check the number of keys
     public float numKey;
     private float keysCollected = 0f;
 
@@ -22,12 +27,22 @@ public class DoorController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (isDoor)
+        if (isDoor && collision.gameObject.CompareTag("Player"))
         {
-
+            if (keysCollected == numKey)
+            {
+                //display the "press E to enter"
+                // if the player presses E the next scene loads with a transition
+                
+            }
+            if (keysCollected != numKey) 
+            { 
+                //display you are missing keys 
+            }
+            
         }
 
-        if (isKey)
+        if (isKey && collision.gameObject.CompareTag("Player"))
         {
 
         }
@@ -35,7 +50,7 @@ public class DoorController : MonoBehaviour
 
     private void finishLevel()
     {
-
+        SceneManager.LoadScene(SceneToLoad);
     }
 
 
