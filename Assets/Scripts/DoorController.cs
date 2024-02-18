964483e1fc56f4516a404c78a331261f2db10d9e
NethulG <157777700+NethulG@users.noreply.github.com>
2024-02-19 10:09:05 +1100

    Update DoorController.cs

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-#pragma warning disable
+
 
 public class DoorController : MonoBehaviour
 {
@@ -15,7 +15,7 @@ public class DoorController : MonoBehaviour
 
     public GameObject LevelTransition;
     
-    private bool withinCollider = false;
+   
     
     
     
