91af14cb6d6d1cacc84737f3594a2982b5522366
NethulG <157777700+NethulG@users.noreply.github.com>
2024-05-30 23:35:08 +1000

    music doneso

--- Assets/Scripts/MusicManagement.cs
+++ Assets/Scripts/MusicManagement.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public static MusicManagement instance;
+
+    
+    public int[] musicScenesIndices = { 0, 1, 2 }; 
+
+    void Awake()
     {
         
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         
+        bool playMusic = false;
+        foreach (int sceneIndex in musicScenesIndices)
+        {
+            if (scene.buildIndex == sceneIndex)
+            {
+                playMusic = true;
+                break;
+            }
+        }
+
+        
+        if (playMusic)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            GetComponent<AudioSource>().Stop();
+        }
     }
 }
+
